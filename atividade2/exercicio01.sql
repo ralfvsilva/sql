44e693ebe5c0be424a1a95ecaa66177be48fb836
Ralf Silva <ralf.silva@mercadolivre.com>
2023-01-25 18:39:45 -0300

    Apagando comentarios desnecessarios.

--- atividade2/exercicio01.sql
+++ atividade2/exercicio01.sql
@@ -46,15 +46,6 @@ VALUES ("Pedro", "Brasil", 2300, 3400, 2);
 INSERT INTO tb_personagens (nome, nacionalidade, ataque, defesa, fk_classes) 
 VALUES ("Flávia", "Brasil", 1900, 1600, 5);
 
-/*
-Faça um SELECT que retorne todes os personagens cujo poder de ataque seja maior do que 2000.
-Faça um SELECT que retorne todes os personagens cujo poder de defesa esteja no intervalo 1000 e 2000.
-Faça um SELECT utilizando o operador LIKE, buscando todes os personagens que possuam a letra C no atributo nome.
-Faça um SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_personagens com os dados da tabela tb_classes.
-Faça um SELECT utilizando a cláusula INNER JOIN, unindo os dados da tabela tb_personagens com os dados da tabela tb_classes, onde traga apenas os personagens que pertençam a uma classe específica (Exemplo: Todes os personagens da classe dos arqueiros).
-Salve todas as queries para cada um dos requisitos do exercício em um único script (arquivo .SQL) e coloque no seu Github pessoal, no repositório que você criou sobre Banco de dados.
-*/
-
 SELECT * FROM tb_personagens WHERE ataque > 2000;
 
 SELECT * FROM tb_personagens WHERE defesa > 1000 AND defesa < 2000;
